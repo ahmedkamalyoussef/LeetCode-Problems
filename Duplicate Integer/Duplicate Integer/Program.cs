e999e4998bdc81b726a157f4f0b2c5e140226381
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2024-08-19 23:51:31 +0300

    new

---
+++ Duplicate Integer/Duplicate Integer/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [1, 2, 3];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var n = 0; n < nums.Length; n++)
+                {
+                    if (nums[i] == nums[n] && i != n)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
