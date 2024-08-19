c77bf2dd4d4c11bd7c1a18613aa852687e7cf8be
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2024-08-20 00:37:05 +0300

    new

--- Is Anagram/Is Anagram/Program.cs
+++ Is Anagram/Is Anagram/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
+            string s, t;
+            if (s.Length != t.Length)
+                return false;
+            foreach (var letter in s)
+            {
+                int count1 = 0;
+                int count2 = 0;
 
+                if (!t.Contains(letter))
+                    return false;
+                foreach (var l in t)
+                {
+                    if (letter == l) count1++;
+                }
+                foreach (var l in s)
+                {
+                    if (letter == l) count2++;
+                }
+                if (count1 != count2) return false;
+            }
+
+            return true;
         }
     }
 }
---
+++ Two Integer Sum/Two Integer Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums, int target;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+}
