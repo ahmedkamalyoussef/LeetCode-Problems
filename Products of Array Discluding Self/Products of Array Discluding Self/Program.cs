7effe7e815238754e34d3f0ea56e6afd6fbfd53c
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2024-08-22 03:53:34 +0300

    Products of Array Discluding Self

---
+++ Products of Array Discluding Self/Products of Array Discluding Self/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new[] { 1, 2, 4, 6 };
+            int[] result = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int multiply = 1;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                        continue;
+                    multiply *= nums[j];
+                }
+                result[i] = multiply;
+            }
+
+            foreach (var VARIABLE in result)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+    }
+}
